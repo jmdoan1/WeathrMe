aa2adbf6e41deea5c419a68580ff884e79b1e17f
Justin Doan <justindoan@justindoan.com>
2017-09-05 20:54:10 -0400

    Remove unnecessary code

--- WeathrMe/Global.swift
+++ WeathrMe/Global.swift
@@ -11,21 +11,3 @@ import CoreLocation
 
 var currentLocation = CLLocationCoordinate2D()
 let apiKey = "68dc4e0dc797bc9e119a8e764a3190f9"
-
-var canGetLocation: Bool {
-    if CLLocationManager.locationServicesEnabled() {
-        return CLLocationManager.authorizationStatus() == .authorizedAlways || CLLocationManager.authorizationStatus() == .authorizedWhenInUse
-    } else {
-        return false
-    }
-}
-
-struct Platform {
-    static let isSimulator: Bool = {
-        var isSim = false
-        #if arch(i386) || arch(x86_64)
-            isSim = true
-        #endif
-        return isSim
-    }()
-}
